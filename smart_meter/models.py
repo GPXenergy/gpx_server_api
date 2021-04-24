b1f6fe22d20fefc24168208cd9958bd5149d2983
Jelle Bouwhuis <jellebouwhuis@outlook.com>
2021-04-24 19:57:26 +0200

    good old mistakes

--- smart_meter/models.py
+++ smart_meter/models.py
@@ -82,7 +82,7 @@ class SmartMeter(models.Model):
         Property if the solar value was updated in the past 2 hours
         :return: bool
         """
-        return self.solar_timestamp and self.solar_timestamp > timezone.now() - timezone.timedelta(hours=2)
+        return self.solar_timestamp and self.gas_timestamp > timezone.now() - timezone.timedelta(hours=2)
 
     @property
     def solar_active(self):
@@ -90,7 +90,7 @@ class SmartMeter(models.Model):
         Property if the gas value was updated in the past 2 hours
         :return: bool
         """
-        return self.gas_timestamp and self.gas_timestamp > timezone.now() - timezone.timedelta(hours=2)
+        return self.gas_timestamp and self.solar_timestamp > timezone.now() - timezone.timedelta(hours=2)
 
     @property
     def last_power_measurement(self):
